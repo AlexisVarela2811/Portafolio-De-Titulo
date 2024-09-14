5f45b91dae3b939b7d4cefa7be7952400e7afa32
Alexis <a.varela@duocuc.cl>
2024-09-14 13:13:37 -0300

    correcion commit

---
+++ Equivale/productos/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from .models import Producto, Categoria
+
+class ProductoForm(forms.ModelForm):
+    class Meta:
+        model = Producto
+        fields = ['nombre', 'descripcion', 'precio', 'stock', 'imagen', 'categoria', 'subcategoria']
+    
+    subcategoria = forms.ModelChoiceField(queryset=Categoria.objects.none(), required=False)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        if 'categoria' in self.data:
+            try:
+                categoria_id = int(self.data.get('categoria'))
+                self.fields['subcategoria'].queryset = Categoria.objects.filter(categoria_padre_id=categoria_id).order_by('nombre')
+            except (ValueError, TypeError):
+                self.fields['subcategoria'].queryset = Categoria.objects.none()
+        
+        elif self.instance.pk:
+            if self.instance.categoria:
+                self.fields['subcategoria'].queryset = Categoria.objects.filter(categoria_padre=self.instance.categoria)
+                if self.instance.subcategoria:
+                    self.fields['subcategoria'].initial = self.instance.subcategoria
+            else:
+                self.fields['subcategoria'].queryset = Categoria.objects.none()
+
